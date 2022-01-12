21ce26d6aa020f5405fbd7e6334a972cc5461b8d
Sklyarenko Philipp <filipp.sklyarenko@mail.ru>
2022-01-13 01:03:31 +0300

    ывывывыв

--- TempConsole/Program.cs
+++ TempConsole/Program.cs
@@ -1,6 +1,6 @@
 ﻿using TempLibrary;
 
-var collection = new MyCollection<int>();
+var collection = new List<int>();
 
 for (int i = 0; i < 500; i++)
 {
@@ -8,10 +8,10 @@ for (int i = 0; i < 500; i++)
     Console.WriteLine(collection[i]);
 }
 
-Console.WriteLine(collection.Lenght);
+Console.WriteLine(collection.Count);
 
-for (int i = 300; i < collection.Lenght; i++)
+for (int i = 300; i < collection.Count; i++)
 {
     collection.RemoveAt(i);
 }
-Console.WriteLine(collection.Lenght);
+Console.WriteLine(collection.Count);
