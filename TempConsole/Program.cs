bf28e7aeb0f1baa06acc75326e014651e648fa5a
Marina <Marina@DESKTOP-DQOV0JK>
2022-01-13 00:48:33 +0300

    Добавьте файлы проекта.

---
+++ TempConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using TempLibrary;
+
+var collection = new MyCollection<int>();
+
+for (int i = 0; i < 500; i++)
+{
+    collection.Add(i);
+    Console.WriteLine(collection[i]);
+}
+
+Console.WriteLine(collection.Lenght);
+
+for (int i = 300; i < collection.Lenght; i++)
+{
+    collection.RemoveAt(i);
+}
+Console.WriteLine(collection.Lenght);
---
+++ TempLibrary/MyCollection.cs
@@ -0,0 +1,43 @@
+﻿namespace TempLibrary
+{
+    public class MyCollection<T>
+    {
+        private T[] Collection;
+        public uint Lenght { get; private set; } = 0;
+
+        public MyCollection(uint lenght = 10)
+        {
+            Collection = new T[lenght];
+        }
+
+        public void Add(T item)
+        {
+            if (Lenght == Collection.Length)
+            {
+                var newCollection = new T[(int)(Collection.Length * 1.5)];
+                Collection.CopyTo(newCollection, 0);
+                Collection = newCollection;
+            }
+            Collection[Lenght++] = item;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= Lenght || index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Lenght--;
+            for (int i = index; i < Lenght; i++)
+            {
+                Collection[i] = Collection[i+1];
+            }
+        }
+
+        public T this[int index]
+        {
+            get { return Collection[index]; }
+            set { Collection[index] = value; }
+        }
+    }
+}
